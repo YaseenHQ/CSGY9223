061f3942db8b7cddd3d062f6fec7c023b10ff028
Sumo-99 <sr7420@nyu.edu>
2026-02-16 17:45:41 -0500

    Update type hints in mappers.py to allow None for channel and message conversion functions

--- components/telegram_client_impl/src/telegram_client_impl/mappers.py
+++ components/telegram_client_impl/src/telegram_client_impl/mappers.py
@@ -4,7 +4,7 @@ from telegram_client_impl.channel import TelegramChannel
 from telegram_client_impl.message import TelegramMessage
 
 
-def to_channel(raw_channel: object) -> TelegramChannel:
+def to_channel(raw_channel: object | None) -> TelegramChannel:
     """Convert a raw Telegram channel/chat object to TelegramChannel."""
     if raw_channel is None:
         msg = "raw_channel cannot be None"
@@ -12,7 +12,7 @@ def to_channel(raw_channel: object) -> TelegramChannel:
     raise NotImplementedError
 
 
-def to_message(raw_message: object) -> TelegramMessage:
+def to_message(raw_message: object | None) -> TelegramMessage:
     """Convert a raw Telegram message object to TelegramMessage."""
     if raw_message is None:
         msg = "raw_message cannot be None"
